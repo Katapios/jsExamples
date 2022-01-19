3536f05688be2f56bb0daf011955f316a99d32d7
Dennis Ryumin <d.ryumin@sigma-it.ru>
2022-01-19 14:29:58 +0300

    simple module import/export

---
+++ module/moduleOne.mjs
@@ -0,0 +1,6 @@
+const sum = (a, b) => a + b
+
+const one = 3
+const two = 2
+
+export { one, two, sum }
---
+++ module/moduleTwo.mjs
@@ -0,0 +1,5 @@
+import { sum as mySumFunc, one, two } from './moduleOne.mjs'
+
+const res = mySumFunc(one, two)
+
+console.log(res)
